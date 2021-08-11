3dcb484d397c68c272924aaadde4eccb91c0f2dc
daranaboza <daranaboza@gmail.com>
2021-08-11 08:14:43 +0200

    Second commit

--- src/views/PruebaView.vue
+++ src/views/PruebaView.vue
@@ -2,12 +2,36 @@
 <template>
   <div>
     <h1>Hola chavales</h1>
+    <br>
+<!--    Al usar 'v-bind:' o ':' en un atributo html, las variables de JS no hacen falta ponerlas entre "{{}}"-->
+<!--    @ == v-on => @mouseover == v-on:mouseover-->
+<!--    : == v-bind:atributo- => :href="value" == v-bind:href="value"-->
+<!--    Mas info => https://es.vuejs.org/v2/guide/events.html -->
+    <input @click="sumaAlBoton" type="button" :value="'Has clickado '+ numeroVeces +' veces'">
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "PruebaView"
+  name: "PruebaView",
+  data(){
+    return{
+      numeroVeces:0
+    }
+  },
+  //Aqí se declaran los métodos o funciones
+  methods:{
+    sumaAlBoton:function (){
+      this.numeroVeces+=1
+    }
+  },
+  //Otro "tipo de funciones" la mayor diferencia es que sus resultados se cachean y solo se ejecutan cuando ha habido un cambio en sus propiedades
+  //Nota: Se puede observar en algunas ocasiones que se usa como si fuera la propiedad "data"
+  // Mas info=> https://es.vuejs.org/v2/guide/computed.html
+  computed:{
+
+  }
 }
 </script>
 
