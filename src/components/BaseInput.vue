3c3003f6ae257e4eeb532d3749fcb870c39bb6a9
daranaboza <daranaboza@gmail.com>
2021-08-17 05:57:50 +0200

    BasicInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+<!--    Se puede bindear desde el padre al hijo de estas 2 formas-->
+<!--    <input type="text" :value="value" @input="returnValue">-->
+    <input type="text" v-model="value" @input="returnValue">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: ['label'],
+  data() {
+    return {
+      value: '',
+    }
+  },
+  methods: {
+    returnValue() {
+      /*
+      Podemos usar eventos personalizados (véase 'pepe') o el evento input prefefinido por 'VUE' que se encarga de recibir el 'value' en el padre y se puede usar
+      con 'v-model'
+       */
+      // this.value = event.target.value
+      // return this.$emit('pepe', this.value)
+      return this.$emit('input',this.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/PruebaView.vue
+++ src/views/PruebaView.vue
@@ -10,15 +10,28 @@
     <input @click="count" type="button" :value="'Has clickado '+ numeroVeces +' veces'">
     <input @click="changeNameUserMutation" type="button" :value="'Cambia nombre en el state'">
     <input @click="changeNameFunctionAction" type="button" :value="'Cambia nombre en el state con un Action'">
+    <br>
+<!--    Uso de evento personalizado para recoger valores de un componente hijo-->
+<!--    <BaseInput label="Prueba"  @pepe="actualiza"></BaseInput>-->
+<!--    Uso de v-model para bindear los datos recibidos desde el componente hijo-->
+    <BaseInput label="Titulo"  v-model="title"></BaseInput>
+    <BaseInput label="Subtitulo"  v-model="subtitle"></BaseInput>
+    <p>{{title}}</p>
+    <p>{{subtitle}}</p>
   </div>
 </template>
 
 <script>
+import BaseInput from "@/components/BaseInput";
 export default {
   name: "PruebaView",
+  components: {BaseInput},
   data() {
     return {
       numeroVeces: 0,
+      title:'',
+      subtitle:''
+
     }
   },
   //Aqí se declaran los métodos o funciones
@@ -35,7 +48,11 @@ export default {
     changeNameFunctionAction() {
       //Esta función hace una llamada a un "action" de 'Vuex' en el 'store', es la manera recomendable de hacer cambios en el 'store'
       this.$store.dispatch('changeNameAction')
-    }
+    },
+    //Ejemplo de uso de función personalizada para actualizar 'title' en la vista
+// actualiza(value){
+//       this.title= value
+// }
   },
   //Otro "tipo de funciones" la mayor diferencia es que sus resultados se cachean y solo se ejecutan cuando ha habido un cambio en sus propiedades, en el caso de los 'state' de 'Vuex' => ver 'computed en NavBar.vue'
   //Nota: Se puede observar en algunas ocasiones que se usa como si fuera la propiedad "data"
@@ -44,7 +61,7 @@ export default {
     //Esto devuelve un state de un modulo en 'store.js'
     nameProduct() {
       return this.$store.state.firstModule.product.name
-    }
+    },
   }
 }
 </script>
